26c9c0804b0c22cf0a0bfb02fa9d7460eb8fe74d
jonas buchholdt <jbuchh13@student.aau.dk>
2017-02-20 14:24:29 +0100

    impedance

--- measure/Matlab/importCSV_inpud_imperdance.m
+++ measure/Matlab/importCSV_inpud_imperdance.m
@@ -1,17 +1,22 @@
 clear all;
-filename = '.csv';
+filename = 'guitar_bridge_picup.csv';
 delimiterIn = ',';
 headerlinesIn = 6;
 A = importdata(filename,delimiterIn,headerlinesIn);
 
 %dat = iddata(A.data(:,1),A.data(:,2));
 
+
+for n = 1:5000
+        ohm(n) = (A.data(n,3)/A.data(n,2))*9980;
+end
+
 figure
-loglog(A.data(:,1),A.data(:,2))
-axis([20 20000 -140 -20])
+loglog(A.data(:,1),ohm(:))
+axis([10 22000 1000 100000])
 set(gca,'fontsize',18)
 hold on
-ylabel('Magnitude [dB]')
+ylabel('Impedance [Ohm]')
 xlabel('Frequency [Hz]')
 hold off
 
