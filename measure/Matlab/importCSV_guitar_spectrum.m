a7a8c759889b8b589d2bd3c0097775f02e1dd5a2
jonas buchholdt <jbuchh13@student.aau.dk>
2017-02-28 15:02:35 +0100

    test edited

--- measure/Matlab/importCSV_guitar_spectrum.m
+++ measure/Matlab/importCSV_guitar_spectrum.m
@@ -1,5 +1,5 @@
 clear all;
-filename = '.csv';
+filename = 'guitar_high_E_flasholet_bridge.csv';
 delimiterIn = ',';
 headerlinesIn = 6;
 A = importdata(filename,delimiterIn,headerlinesIn);
@@ -11,7 +11,8 @@ loglog(A.data(:,1),A.data(:,2))
 axis([20 20000 -140 -20])
 set(gca,'fontsize',18)
 hold on
-ylabel('Magnitude [dB]')
+grid on
+ylabel('Magnitude [dBu]')
 xlabel('Frequency [Hz]')
 hold off
 
